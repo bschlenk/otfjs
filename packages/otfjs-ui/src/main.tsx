8966296ec034562bb8cdc328e2d5f79ceb78016f
Brian Schlenker <bschlenk@umich.edu>
2024-06-04 15:44:18 -0400

    start extracting color variables

--- packages/otfjs-ui/src/main.tsx
+++ packages/otfjs-ui/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 import { App } from './app.tsx'
 
+import './styles/colors.css'
 import './main.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
