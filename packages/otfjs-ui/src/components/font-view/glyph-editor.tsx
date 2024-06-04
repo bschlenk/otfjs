c7e66c368c6a28ce293d0648df5e054a35f4bfc8
Brian Schlenker <bschlenk@umich.edu>
2024-06-04 15:45:49 -0400

    fix glyph preview

--- packages/otfjs-ui/src/components/font-view/glyph-editor.tsx
+++ packages/otfjs-ui/src/components/font-view/glyph-editor.tsx
@@ -8,7 +8,7 @@ import {
   useState,
 } from 'react'
 import { GlyphSimple, glyphToSvgPath, renderGlyphToCanvas } from 'otfjs'
-import { vec } from 'otfjs/util'
+import { vec, Vector } from 'otfjs/util'
 
 import { usePrevious } from '../../hooks/use-previous'
 import { relativeMouse } from '../../utils/event'
@@ -25,45 +25,55 @@ export function GlyphEditor({
   const ref = useRef<SVGSVGElement>(null)
   const size = useSize(ref)
 
-  // const center = useMemo(() => centeredMatrix(glyph, size), [glyph, size])
-  const { origin, scale: s } = useOriginScale(ref as any /*, center */)
-  const s2 = 16 / ppem
+  const center = useMemo(() => centeredGlyph(glyph, size), [glyph, size])
+  const { origin, scale: s } = useOriginScale(ref as any, center)
+  const gScale = 16 / ppem
+
+  console.log(origin, s, glyph.xMax - glyph.xMin, glyph.yMax - glyph.yMin)
 
   const x = origin.x
   const y = origin.y
 
   const d = glyphToSvgPath(glyph)
 
-  const gCanvas = useMemo(
-    () => renderGlyph(glyph, { scale: s2, antialias: false }),
-    [glyph, s2],
+  const [gCanvas, gOffset] = useMemo(
+    () => renderGlyph(glyph, { scale: gScale, antialias: true }),
+    [glyph, gScale],
   )
 
   const canvas = useCallback(
     (canvas: HTMLCanvasElement | null) => {
       if (!canvas) return
 
-      canvas.width = size.x
-      canvas.height = size.y
+      const dpi = window.devicePixelRatio
+
+      canvas.width = size.x * dpi
+      canvas.height = size.y * dpi
+
+      const ox = gOffset.x
+      const oy = gOffset.y
 
       const ctx = canvas.getContext('2d')!
       ctx.imageSmoothingEnabled = false
       ctx.clearRect(0, 0, canvas.width, canvas.height)
-      ctx.transform(s / s2, 0, 0, s / s2, x, y)
+      ctx.scale(dpi, dpi)
+      ctx.transform(s / gScale, 0, 0, -s / gScale, x, y + glyph.yMax * s)
 
       ctx.globalAlpha = 0.75
-      ctx.drawImage(gCanvas, 0, 0)
+      ctx.drawImage(gCanvas, ox, oy)
+
+      if (s < 0.075) return
 
       // for each px, clear a 1px line so we can see the individual pixels
-      for (let i = 0; i < gCanvas.width; ++i) {
-        ctx.clearRect(i, 0, (1 * s2) / s, gCanvas.height)
+      for (let i = 1; i < gCanvas.width; ++i) {
+        ctx.clearRect(i + ox, 0, gScale / s, gCanvas.height + oy)
       }
 
-      for (let i = 0; i < gCanvas.height; ++i) {
-        ctx.clearRect(0, i, gCanvas.width, (1 * s2) / s)
+      for (let i = 1; i < gCanvas.height; ++i) {
+        ctx.clearRect(0, i + oy, gCanvas.width + ox, gScale / s)
       }
     },
-    [gCanvas, s, s2, size, x, y],
+    [size, gOffset, s, gScale, x, y, glyph.yMax, gCanvas],
   )
 
   return (
@@ -92,19 +102,22 @@ export function GlyphEditor({
               stroke="red"
               strokeWidth={0.5 / s}
             />
-            <circle cx={0} cy={0} r={4 / s} fill="blue" />
-            <path d={d} stroke="var(--color-icon)" strokeWidth={1 / s} />
-            {glyph.points.map((p, i) => (
-              <circle
-                key={i}
-                cx={p.x}
-                cy={p.y}
-                r={p.onCurve ? 4 / s : 3 / s}
-                fill={p.onCurve ? 'var(--color-icon)' : 'none'}
-                strokeWidth={2 / s}
-                stroke={p.onCurve ? undefined : 'var(--color-icon)'}
-              />
-            ))}
+            {s >= 0.075 && <circle cx={0} cy={0} r={4 / s} fill="blue" />}
+            {s >= 0.075 && (
+              <path d={d} stroke="var(--color-icon)" strokeWidth={1 / s} />
+            )}
+            {s >= 0.075 &&
+              glyph.points.map((p, i) => (
+                <circle
+                  key={i}
+                  cx={p.x}
+                  cy={p.y}
+                  r={p.onCurve ? 4 / s : 3 / s}
+                  fill={p.onCurve ? 'var(--color-icon)' : 'none'}
+                  strokeWidth={2 / s}
+                  stroke={p.onCurve ? undefined : 'var(--color-icon)'}
+                />
+              ))}
           </g>
         </g>
       </svg>
@@ -193,10 +206,9 @@ function useOriginScale(
   return originScale
 }
 
-// const MARGIN = 32
+const MARGIN = 32
 
-/*
-function centeredMatrix(glyph: GlyphSimple, size: Vector) {
+function centeredGlyph(glyph: GlyphSimple, size: Vector) {
   const { xMin, xMax, yMin, yMax } = glyph
   const width = xMax - xMin
   const height = yMax - yMin
@@ -206,28 +218,27 @@ function centeredMatrix(glyph: GlyphSimple, size: Vector) {
 
   const sx = sw / width
   const sy = sh / height
-  const s = 1 / Math.min(sx, sy)
+  const s = Math.min(sx, sy)
 
   let x = 0
   let y = 0
 
   if (sy > sx) {
-    x = xMin - MARGIN * s
-    y = -(size.y * s - height) / 2
+    x = MARGIN - xMin * s
+    y = (size.y - height * s) / 2 - yMin * s
   } else {
-    x = -(size.x * s - width) / 2
-    y = yMin - MARGIN * s
+    x = (size.x - width * s) / 2 - xMin * s
+    y = MARGIN
   }
 
-  return mat.mat(s, 0, 0, s, x, y)
+  return { origin: { x, y }, scale: s }
 }
-*/
 
 const FILTER_ANTI_ALIAS = `url('data:image/svg+xml,\
 <svg xmlns="http://www.w3.org/2000/svg">\
 <filter id="f" color-interpolation-filters="sRGB">\
 <feComponentTransfer>\
-<feFuncA type="discrete" tableValues="0 0 0 1"/>\
+<feFuncA type="discrete" tableValues="0 0 0 0 0 1 1 1"/>\
 </feComponentTransfer>\
 </filter>\
 </svg>#f')`
@@ -246,8 +257,8 @@ function renderGlyph(
   const gWidth = glyph.xMax - glyph.xMin
   const gHeight = glyph.yMax - glyph.yMin
 
-  canvas.width = Math.ceil(gWidth * scale)
-  canvas.height = Math.ceil(gHeight * scale)
+  canvas.width = Math.ceil(gWidth * scale) + 2
+  canvas.height = Math.ceil(gHeight * scale) + 2
 
   const ctx = canvas.getContext('2d')!
 
@@ -255,10 +266,13 @@ function renderGlyph(
     ctx.filter = FILTER_ANTI_ALIAS
   }
 
-  ctx.scale(scale, scale)
-  ctx.transform(1, 0, 0, -1, 0, glyph.yMax)
+  const ox = -Math.floor(glyph.xMin * scale) + 1
+  const oy = -Math.floor(glyph.yMin * scale) + 1
+
+  ctx.transform(scale, 0, 0, scale, ox, oy)
+
   renderGlyphToCanvas(glyph, ctx)
   ctx.fill()
 
-  return canvas
+  return [canvas, { x: -ox, y: -oy }] as const
 }
