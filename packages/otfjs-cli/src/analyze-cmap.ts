090a480a905dc7eaa95d3db5128cc1a88982c33b
Brian Schlenker <bschlenk@umich.edu>
2024-07-05 12:06:51 -0400

    add analyze-cmap script

---
+++ packages/otfjs-cli/src/analyze-cmap.ts
@@ -0,0 +1,47 @@
+import fs from 'fs/promises'
+import { Font } from 'otfjs'
+import path from 'path'
+
+const args = process.argv.slice(2)
+if (args.length < 1) {
+  console.error('usage: analyze-cmap <font-dir>')
+  process.exit(1)
+}
+
+const fontDir = args[0]
+const fnames = await fs.readdir(fontDir)
+
+await analyzeCmap()
+
+async function analyzeCmap() {
+  const platformEncodings = new Map<string, number>()
+
+  for (const fname of fnames) {
+    if (fname.startsWith('.')) continue
+
+    try {
+      const data = await fs.readFile(path.join(fontDir, fname))
+      const font = new Font(data.buffer)
+
+      const cmap = font.getTable('cmap')
+      for (const record of cmap.encodingRecords) {
+        const key = `${record.platformId}-${record.encodingId}`
+        const count = platformEncodings.get(key) || 0
+        platformEncodings.set(key, count + 1)
+
+        if (record.platformId === 1) {
+          console.log(fname, record.platformId, record.encodingId)
+        }
+      }
+    } catch (e) {
+      console.error('something went wrong processing', fname)
+      throw e
+    }
+  }
+
+  Array.from(platformEncodings.entries())
+    .sort((a, b) => b[1] - a[1])
+    .forEach(([pe, count]) => {
+      console.log(pe, count)
+    })
+}
