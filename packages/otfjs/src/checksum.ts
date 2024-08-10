4c1319174cb26f5161472414790af10f8e0bae29
Brian Schlenker <bschlenk@umich.edu>
2024-08-09 22:13:18 -0400

    handle non multiples of 4 in checksum

--- packages/otfjs/src/checksum.ts
+++ packages/otfjs/src/checksum.ts
@@ -1,9 +1,20 @@
 export function computeChecksum(table: DataView) {
   let checksum = 0
-  let o = 0
-  while (o < table.byteLength) {
-    checksum = (checksum + table.getUint32(o)) >>> 0
-    o += 4
+
+  let n = table.byteLength
+  const remainder = n % 4
+  n -= remainder
+
+  for (let i = 0; i < n; i += 4) {
+    checksum = (checksum + table.getUint32(i)) >>> 0
+  }
+
+  if (remainder) {
+    // Read the final u32 padded with zeros
+    const b = new Uint8Array(4)
+    const d = new DataView(b.buffer)
+    b.set(new Uint8Array(table.buffer, n, remainder))
+    checksum = (checksum + d.getUint32(0)) >>> 0
   }
 
   return checksum
