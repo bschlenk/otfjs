35f2c08f2d01226910daae96d0f085f4ddfb2276
Brian Schlenker <bschlenk@umich.edu>
2025-04-24 22:30:52 -0400

    fix complex previews

--- packages/otfjs-ui/src/components/font-icon/font-icon.tsx
+++ packages/otfjs-ui/src/components/font-icon/font-icon.tsx
@@ -1,10 +1,28 @@
+import complexFontsArray from '../../fonts-complex.json'
+
+import styles from './font-icon.module.css'
+
 export interface FontIconProps {
   name: string
   size: number
 }
 
+const complexFonts = new Set(complexFontsArray)
+
 export function FontIcon({ name, size }: FontIconProps) {
   const id = name.toLowerCase().replaceAll(' ', '-')
+
+  if (complexFonts.has(id)) {
+    return (
+      <img
+        src={`${id}.svg`}
+        width={size}
+        height={size}
+        className={styles.imgPreview}
+      />
+    )
+  }
+
   return (
     <svg width={size} height={size} fill="var(--color-text)">
       <use href={`preview.svg#${id}`} />
