739cecdaf43b34dd9eaf02e822b29ef7f1e46760
Brian Schlenker <bschlenk@umich.edu>
2025-06-03 22:55:45 -0400

    start working on it

--- packages/otfjs-ui/vite.config.ts
+++ packages/otfjs-ui/vite.config.ts
@@ -1,9 +1,13 @@
 import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    react(),
+  ],
   build: {
     // based on figma's browser support matrix
     target: ['chrome99', 'firefox101', 'safari16'],
