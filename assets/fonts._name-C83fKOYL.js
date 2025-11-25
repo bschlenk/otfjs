0e8f0e436fdbd80f3a33647d19f147c5a21b17a5
bschlenk <bschlenk@umich.edu>
2025-11-25 04:43:52 +0000

    Deploying to gh-pages from @ bschlenk/otfjs@76d04cac10d12c9871dc5bdc8c39e997531eb44f 🚀

---
+++ assets/fonts._name-C83fKOYL.js
@@ -0,0 +1,1 @@
+import{R as t,j as n}from"./index-Cnmeg6cX.js";import{a as e}from"./font-view-Cid63MDo.js";function a(){const o=t.useLoaderData();return n.jsx(e,{font:o})}export{a as component};
--- assets/fonts._name-CzHTRWE6.js
+++
@@ -1,1 +0,0 @@
-import{R as t,j as n}from"./index-C6mbSGQf.js";import{a as e}from"./font-view-Bgg2B42q.js";function a(){const o=t.useLoaderData();return n.jsx(e,{font:o})}export{a as component};
